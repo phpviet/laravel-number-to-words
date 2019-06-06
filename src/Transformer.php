44fa721192c89acbf86ff3530913ed12124dc704
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-06 23:25:50 +0700

    Added macro support for transformer

---
+++ src/Transformer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link https://github.com/phpviet/laravel-number-to-words
+ *
+ * @copyright (c) PHP Viet
+ * @license [MIT](https://opensource.org/licenses/MIT)
+ */
+
+namespace PHPViet\Laravel\NumberToWords;
+
+use Illuminate\Support\Traits\Macroable;
+use PHPViet\NumberToWords\Transformer as BaseTransformer;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+class Transformer extends BaseTransformer
+{
+
+    use Macroable;
+
+}
